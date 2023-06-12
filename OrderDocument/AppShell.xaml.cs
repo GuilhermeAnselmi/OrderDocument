128b9f311efae1950afedcf09386c200427772ac
Guilherme Anselmi <60523666+GuilhermeAnselmi@users.noreply.github.com>
2023-06-12 13:32:19 -0300

    Add project files.

---
+++ OrderDocument/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace OrderDocument;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ OrderDocument/Temp.xaml.cs
@@ -0,0 +1,9 @@
+namespace OrderDocument;
+
+public partial class Temp : ContentPage
+{
+	public Temp()
+	{
+		InitializeComponent();
+	}
+}
